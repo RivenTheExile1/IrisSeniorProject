6eb9a80d7a0a32df49555752d34c26ce79b578bf
RivenTheExile1 <connor.a.ferwerda@gmail.com>
2021-03-23 23:58:49 -0400

    got searching to wrok. can't get movement to work 3/23

--- Simulation/iris_vision_simulation.py
+++ Simulation/iris_vision_simulation.py
@@ -1,55 +1,105 @@
 import pygame
 import sys
+import time
 import random
 
-class set_up:
-
-    #initing the main variabgle we will need
-    def main():
-
-        #init rng
-        random.seed()
-
-        #initing clock and pygame
-        pygame.init()
-        clock = pygame.time.Clocck()
-
-        #setting fps, size of screen and backround color
-        fps = 60
-        size = [600,600]
-        bg = [0,0,0]
-
-        #init screen
-        screen = pygame.display.set_mode(size)
-
-        #init drone
-        drone = pygame.Rect(20,20,0,0)
-
-        #init target and random loc from 0,600 (size of screen)
-        target = pygame.Rect(5,5,random.randint(0,600), random.randint(0,600))
 
 
-        #loop that runs game
-        while True:
-            
-            #if something breaks then we out
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit
-                    return False
+
+def search(screen, drone, target):
+    #search func
+    
+    #iniate at start but will update
+    
+    x = 10
+    y = 10
+    next_x = x
+    next_y = y
 
 
-            #set up actual "game"
-            screen.fill(bg)
 
-            #draws drone and target
-            pygame.draw.rect(screen, [255,0,0], drone)
-            pygame.draw.rect(screen, [0,0,255], target)
-           
-           
+    #search nearby area
+    for i in range(0,600,20): #y 
+        next_y = y + i 
+        for j in range(0,600,20): #x 
+            next_x = x + j 
+            pixel_col = pygame.Surface.get_at(screen, (next_x, next_y))
+            print (pixel_col)
+            if pixel_col == (0,0,255,255):
+                print ("!!!!!!!!!!!!!!!!found blue at ", next_x, next_y)
+            else:
+                    print ("no blue at ", next_x, next_y)
+            #moves drone over if not at 600, or next line if at 600
+
+            if next_x == 600:
+                pygame.Rect.move(drone, 0,next_y + 20)
+            else: 
+                pygame.Rect.move(drone, (next_x, next_y))
+
+            time.sleep(.5)
+
             pygame.display.update()
-            clock.tick(fps)
+            
+            
 
-if __name__ == '__main__':
-    set_up.main()
+            
+
+#initing the main variabgle we will need
+def set_up():
+
+    #init rng
+    random.seed()
+
+    #initing clock and pygame
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    #setting fps, size of screen and backround color
+    fps = 60
+    size = [600,600]
+    bg = [255,255,255]
+
+    #init screen
+    screen = pygame.display.set_mode(size)
+
+    #init drone
+    drone = pygame.Rect(0,0,20,20)
+
+    #init target and random loc from 0,600 (size of screen)
+    target_x = random.randint(0,600)
+    target_y = random.randint(0,600)
+    print("target loc", target_x, target_y)
+    target = pygame.Rect(target_x, target_y,20, 20)
+
+
+    #loop that runs game
+    while True:
+        
+        #if something breaks then we out
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit
+                return False
+
+
+        #set up actual "game"
+        screen.fill(bg)
+
+        #draws drone and target
+        pygame.draw.rect(screen, [255,0,0], drone)
+        pygame.draw.rect(screen, [0,0,255], target)
+
+        
+        
+        
+            
+
+        
+        #update screen
+        pygame.display.update()
+        clock.tick(fps)
+        search(screen, drone, target)
+        
+
+set_up()     
